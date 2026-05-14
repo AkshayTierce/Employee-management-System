6769d24d7b5cb857ab052eee061c64361737715e
Akshay ChaudharY <akshaychaudhary@Akshays-MacBook-Air.local>
2026-05-14 11:34:55 +0530

    import Errors Solve

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import path from 'path';
+import path from 'path'
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 import express from 'express';
 import connectDB from "./config/db.ts"
-import cors from 'cors';
+import cors from "cors"
 import empRouter from './routes/employeeRoutes.ts';
 const app = express();
 const port = process.env.PORT || 3001;
